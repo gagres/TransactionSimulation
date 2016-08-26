e079354a9707d52e03be4764e2ea02f879163107
gagres <gabriel.giro.resende@outlook.com>
2016-08-26 01:26:30 -0300

    Atualizando...

--- js/controllers/transactionCtrl.js
+++ js/controllers/transactionCtrl.js
@@ -8,8 +8,8 @@
 
     function transactionCtrl(){
       var vm = this;
-
       vm.field_errors = "Não foi possível fazer isso";
+      vm.amount = 0;
       vm.card = {
         "number": null,
         "holder_name": null,
@@ -17,19 +17,31 @@
         "cvv": null
       };
       vm.products = [
-        {'url':'img/feijao.jpg', 'name': 'Feijao', 'price': 14},
-        {'url':'img/arroz.jpg', 'name': 'Arroz', 'price': 6},
-        {'url':'img/cheetos.jpg', 'name': 'Salgadinho Cheetos', 'price': 3},
-        {'url':'img/friboi.jpg', 'name': 'Carne Friboi', 'price': 8.75}
+        {'url':'img/feijao.jpg', 'name': 'Feijao', 'price': 14, 'qte': 0},
+        {'url':'img/arroz.jpg', 'name': 'Arroz', 'price': 6, 'qte': 0},
+        {'url':'img/cheetos.jpg', 'name': 'Salgadinho Cheetos', 'price': 3, 'qte': 0},
+        {'url':'img/friboi.jpg', 'name': 'Carne Friboi', 'price': 8.75, 'qte': 0}
       ]
 
       vm.confirmTransaction = confirmTransaction;
+      vm.increaseAmount = increaseAmount;
 
       function confirmTransaction(card){
         if(card.hasOwnProperty('expiration_month'))
           console.log(new Date(card.expiration_month).getMonth(), new Date(card.expiration_month).getFullYear());
 
         console.log(card);
+      }
+
+      function increaseAmount(product, qte){
+        if(product.qte == 0 && parseInt(qte) < 0){
+          //Do nothing
+        }else{
+          if(product.qte >= 0){
+            vm.amount += product.price * parseInt(qte);
+            product.qte += parseInt(qte);
+          }
+        }
       }
     }
 })();
